bc73545f960e1dcf46a41f48dc9fce85bb9c3a8e
Rachel Wyatt <rw126@evansville.edu>
2018-12-18 12:43:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 class App < Sinatra::Base
 
-  # Define your GET '/' route below and respond with "Hello, World!"
+  require_relative 'config/environment'
+
+  class App < Sinatra::Base
+
+    get '/' do
+      "Hello, world!"
+    end
+
+  end
+
 
 
 end
